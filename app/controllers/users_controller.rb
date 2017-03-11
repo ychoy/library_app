50036fcb2f577a1a82aa5cafae1069eb2f85d261
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-11 13:03:54 -0800

    new and create users path and view generated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
 end
