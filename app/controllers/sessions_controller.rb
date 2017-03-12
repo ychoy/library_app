557445f9a383c56f50fbe363b2bb78f3551c8137
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-11 16:21:01 -0800

    created logged out path and flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,17 @@ class SessionsController < ApplicationController
     @user = User.confirm(user_params)
     if @user
       login(@user)
+      flash[:notice] = "Successfully logged in."
       redirect_to @user
     else
+      flash[:error] = "Incorrect email or password."
       redirect_to login_path
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Successfully logged out."
+    redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,10 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logout
+    @current_user = session[:user_id] = nil
   end
 end
