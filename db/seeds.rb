e63c918b776448d309fbce45a5d9aeb57e6a8e53
Yan Yin Choy <yanyinchoy@gmail.com>
2017-03-13 01:47:14 -0700

    updated db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-User.delete_all
+User.destroy_all
 
 User.create({
   first_name: 'Tombo',
@@ -14,3 +14,38 @@ User.create({
   email: 'tombo@inventor.com',
   password: 'helloworld'
 })
+
+User.create({
+  first_name: 'Barry',
+  last_name: 'Obama',
+  email: 'barry@obama.org',
+  password: 'strangerthings'
+})
+
+User.create({
+  first_name: 'Crystal',
+  last_name: 'Teicho',
+  email: 'crystal@me.com',
+  password: 'hellohello'
+})
+
+User.create({
+  first_name: 'Erik',
+  last_name: 'Duval',
+  email: 'duval@me.com',
+  password: 'worldhello'
+})
+
+Library.destroy_all
+
+Library.create({
+  name: 'Dr. Martin Luther King Jr. Library',
+  floor_count: 9,
+  floor_area: 6708
+ })
+
+Library.create({
+  name: 'Joyce Ellington Library',
+  floor_count: 2,
+  floor_area: 568
+ })
